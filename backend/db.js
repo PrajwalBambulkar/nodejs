b38844684a25a4335e4a2be200046d0024cef327
PrajwalBambulkar <109361179+PrajwalBambulkar@users.noreply.github.com>
2025-06-03 12:01:22 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,21 @@
+// db.js
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'mysql-container',
-    user: 'root',
-    password: 'yourpassword',
-    database: 'testdb',
-    port: 3306
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'yourpassword',
+    database: process.env.DB_NAME || 'testdb',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
 });
 
 connection.connect(err => {
     if (err) {
         console.error('MySQL connection error:', err);
-        process.exit(1);  // optional: stop app if DB connection fails
+        process.exit(1);
     } else {
         console.log('Connected to MySQL database');
     }
 });
 
 module.exports = connection;
-
